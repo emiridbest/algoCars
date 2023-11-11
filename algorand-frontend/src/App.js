d7595f4b336ef75ec302bb208f48a9249c196f5b
emiridbest <udogwuemiri@gmail.com>
2023-11-11 13:10:28 +0100

    second commit

---
+++ algorand-frontend/src/App.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+
+import Cover from "./components/Cover";
+
+import "./App.css";
+
+import Wallet from "./components/Wallet";
+
+import { Container, Nav } from "react-bootstrap";
+
+// import Products from "./components/marketplace/Products";
+
+// import {Notification} from "./components/utils/Notifications";
+
+import { indexerClient, myAlgoConnect } from "./components/utils/constant";
+
+import coverImg from "./assets/img/car.png";
+
+import Products from "./components/marketplace/Products";
+
+import { Notification } from "./components/utils/Notifications";
+
+const App = function AppWrapper() {
+  const [address, setAddress] = useState(null);
+
+  const [name, setName] = useState(null);
+
+  const [balance, setBalance] = useState(0);
+
+  const fetchBalance = async (accountAddress) => {
+    indexerClient
+      .lookupAccountByID(accountAddress)
+      .do()
+
+      .then((response) => {
+        const _balance = response.account.amount;
+
+        setBalance(_balance);
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const connectWallet = async () => {
+    myAlgoConnect
+      .connect()
+
+      .then((accounts) => {
+        const _account = accounts[0];
+
+        setAddress(_account.address);
+
+        setName(_account.name);
+
+        fetchBalance(_account.address);
+      })
+      .catch((error) => {
+        console.log("Could not connect to MyAlgo wallet");
+
+        console.error(error);
+      });
+  };
+
+  const disconnect = () => {
+    setAddress(null);
+
+    setName(null);
+
+    setBalance(null);
+  };
+
+  //..
+
+  return (
+    <>
+      <Notification />
+
+      {address ? (
+        <Container fluid="md">
+          <Nav className="justify-content-end pt-3 pb-5">
+            <Nav.Item>
+              <Wallet
+                address={address}
+                name={name}
+                amount={balance}
+                disconnect={disconnect}
+                symbol={"ALGO"}
+              />
+            </Nav.Item>
+          </Nav>
+
+          <main>
+            <Products address={address} fetchBalance={fetchBalance} />{" "}
+          </main>
+        </Container>
+      ) : (
+        <Cover
+          name={"Algo Cars"}
+          coverImg={coverImg}
+          connect={connectWallet}
+        />
+      )}
+    </>
+  );
+};
+
+export default App;
