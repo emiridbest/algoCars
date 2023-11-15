25ef7e85db7f0a247206e991bd55569ac0e97743
Ujayata <62179207+Ujayata@users.noreply.github.com>
2023-11-15 06:18:44 +0100

    Update marketplace_contract.py
    **Errors:**
    
    1. **`Btoi(Txn.application_args[3]) > Int(0)`:** This assertion checks if the price is greater than zero. However, it should also check if the price is a valid integer value. Currently, it would accept non-integer values, such as strings, which could lead to unexpected behavior.
    
    **Vulnerabilities:**
    
    1. **Lack of input validation:** The code does not properly validate all user inputs. For instance, it doesn't check if the product name, image, or description are valid strings. This could allow attackers to inject malicious code or data into the application.
    
    2. **Insufficient protection against unauthorized access:** The `application_deletion()` function only checks if the transaction sender is the creator address. However, this doesn't ensure that the sender has the necessary authorization to delete the application. A malicious user could potentially gain control of the creator address and delete the application without proper authorization.
    
    **Bugs:**
    
    1. **`valid_number_of_transactions = Global.group_size() == Int(2)`:** This assertion checks if there are exactly two transactions in the group. However, this might not be the intended behavior. If the application allows buying multiple products in a single transaction, this assertion would prevent such transactions.
    
    **Suggested Solutions:**
    
    1. **Refine input validation:** Implement stricter input validation for all user inputs. This includes checking for valid data types, lengths, and content.
    
    2. **Introduce access control mechanisms:** Implement role-based access control (RBAC) to restrict application deletion to authorized users. Verify the sender's authorization before allowing the application to be deleted.
    
    3. **Modify transaction count assertion:** Adjust the transaction count assertion to allow for a valid number of transactions based on the application's intended functionality.
    
    4. **Thorough testing:** Conduct rigorous testing to ensure the application's correctness and security. Utilize automated testing tools and manual testing to identify and address any potential issues.

--- marketplace_contract.py
+++ marketplace_contract.py
@@ -1,7 +1,5 @@
 from pyteal import *
 
-
-
 class Product:
     class Variables:
         name = Bytes("NAME")
@@ -13,11 +11,22 @@ class Product:
     class AppMethods:
         buy = Bytes("buy")
 
+    def validate_string_input(self, string_input):
+        return And(
+            Gtxn[0].application_id() == Int(0),
+            Gtxn[0].application_args[0] == Bytes("tutorial-marketplace:uv1"),
+            Gtxn[0].application_args[1] == self.AppMethods.buy,
+            Gtxn[0].application_args[2] == String(""),
+            Gtxn[0].application_args[3] == String(""),
+        )
 
     def application_creation(self):
         return Seq([
             Assert(Txn.application_args.length() == Int(4)),
-            Assert(Txn.note() == Bytes("tutorial-marketplace:uv1")),
+            Assert(self.validate_string_input(Txn.application_args[0])),
+            Assert(self.validate_string_input(Txn.application_args[1])),
+            Assert(self.validate_string_input(Txn.application_args[2])),
+            Assert(self.validate_string_input(Txn.application_args[3])),
             Assert(Btoi(Txn.application_args[3]) > Int(0)),
             App.globalPut(self.Variables.name, Txn.application_args[0]),
             App.globalPut(self.Variables.image, Txn.application_args[1]),
@@ -27,39 +36,35 @@ class Product:
             Approve()
         ])
 
-
     def buy(self):
         count = Txn.application_args[1]
-        valid_number_of_transactions = Global.group_size() == Int(2)
+        valid_number_of_transactions = Global.group_size() >= Int(1)
         valid_payment_to_seller = And(
             Gtxn[1].type_enum() == TxnType.Payment,
             Gtxn[1].receiver() == Global.creator_address(),
             Gtxn[1].amount() == App.globalGet(self.Variables.price) * Btoi(count),
             Gtxn[1].sender() == Gtxn[0].sender(),
         )
-        can_buy = And(valid_number_of_transactions,
-                      valid_payment_to_seller)
+        can_buy = And(valid_number_of_transactions, valid_payment_to_seller)
         update_state = Seq([
             App.globalPut(self.Variables.sold, App.globalGet(self.Variables.sold) + Btoi(count)),
             Approve()
         ])
         return If(can_buy).Then(update_state).Else(Reject())
 
-
     def application_deletion(self):
-        return Return(Txn.sender() == Global.creator_address())
-
+        authorized_deletion = And(
+            Gtxn[0].sender() == Global.creator_address(),
+            Gtxn[0].application_args[0] == Bytes("delete_app")
+        )
+        return Return(authorized_deletion)
 
     def application_start(self):
-
         return Cond(
-
             [Txn.application_id() == Int(0), self.application_creation()],
             [Txn.on_completion() == OnComplete.DeleteApplication, self.application_deletion()],
             [Txn.application_args[0] == self.AppMethods.buy, self.buy()]
-
         )
-
 
     def approval_program(self):
         return self.application_start()
