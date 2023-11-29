3e91160a59aade3406922117b86753b0df3008fb
fitihamlak <150477997+fitihamlak@users.noreply.github.com>
2023-11-29 20:11:46 +0300

    Update marketplace_contract.py

--- marketplace_contract.py
+++ marketplace_contract.py
@@ -1,8 +1,8 @@
 from pyteal import *
 
 
-
 class Product:
+
     class Variables:
         name = Bytes("NAME")
         image = Bytes("IMAGE")
@@ -12,7 +12,57 @@ class Product:
 
     class AppMethods:
         buy = Bytes("buy")
+        updatePrice = Bytes("updatePrice")
 
+    def input_validation(self, method):
+        if method == self.AppMethods.buy:
+            valid_args = And(
+                Txn.application_args.length() == Int(4),
+                Txn.application_args[0] == self.AppMethods.buy,
+                Btoi(Txn.application_args[3]) > Int(0)
+            )
+            return valid_args
+        elif method == self.AppMethods.updatePrice:
+            valid_args = And(
+                Txn.application_args.length() == Int(2),
+                Txn.application_args[0] == self.AppMethods.updatePrice,
+                Btoi(Txn.application_args[1]) > Int(0)
+            )
+            return valid_args
+        else:
+            return False
+
+    def authentication(self, method):
+        if method == self.AppMethods.updatePrice:
+            valid_sender = Txn.sender() == Global.creator_address()
+            return valid_sender
+        else:
+            return True
+
+    def buy(self):
+        count = Btoi(Txn.application_args[1])
+        valid_number_of_transactions = Global.group_size() == Int(2)
+        valid_payment_to_seller = And(
+            Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[1].receiver() == Global.creator_address(),
+            Gtxn[1].amount() == App.globalGet(self.Variables.price) * count,
+            Gtxn[1].sender() == Gtxn[0].sender(),
+        )
+        can_buy = And(valid_number_of_transactions,
+                      valid_payment_to_seller)
+        update_state = Seq([
+            App.globalPut(self.Variables.sold, App.globalGet(self.Variables.sold) + count),
+            Approve()
+        ])
+        return If(can_buy).Then(update_state).Else(Reject())
+
+    def updatePrice(self):
+        new_price = Btoi(Txn.application_args[1])
+        if new_price > Int(0):
+            App.globalPut(self.Variables.price, new_price)
+            return Approve()
+        else:
+            return Reject()
 
     def application_creation(self):
         return Seq([
@@ -27,39 +77,24 @@ class Product:
             Approve()
         ])
 
-
-    def buy(self):
-        count = Txn.application_args[1]
-        valid_number_of_transactions = Global.group_size() == Int(2)
-        valid_payment_to_seller = And(
-            Gtxn[1].type_enum() == TxnType.Payment,
-            Gtxn[1].receiver() == Global.creator_address(),
-            Gtxn[1].amount() == App.globalGet(self.Variables.price) * Btoi(count),
-            Gtxn[1].sender() == Gtxn[0].sender(),
-        )
-        can_buy = And(valid_number_of_transactions,
-                      valid_payment_to_seller)
-        update_state = Seq([
-            App.globalPut(self.Variables.sold, App.globalGet(self.Variables.sold) + Btoi(count)),
-            Approve()
-        ])
-        return If(can_buy).Then(update_state).Else(Reject())
-
-
     def application_deletion(self):
         return Return(Txn.sender() == Global.creator_address())
 
-
     def application_start(self):
+        method = Txn.application_args[0]
 
-        return Cond(
+        if not self.input_validation(method):
+            return Reject()
 
-            [Txn.application_id() == Int(0), self.application_creation()],
-            [Txn.on_completion() == OnComplete.DeleteApplication, self.application_deletion()],
-            [Txn.application_args[0] == self.AppMethods.buy, self.buy()]
+        if not self.authentication(method):
+            return Reject()
 
-        )
-
+        if method == self.AppMethods.buy:
+            return self.buy()
+        elif method == self.AppMethods.updatePrice:
+            return self.updatePrice()
+        else:
+            return Reject()
 
     def approval_program(self):
         return self.application_start()
